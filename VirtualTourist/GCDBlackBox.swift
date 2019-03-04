188b739d75f69679c17528ecde196914d90708d7
Shane Sealy <35928028+GSD4life@users.noreply.github.com>
2019-03-04 13:22:42 -0500

    Added GCDBlackBox file.

---
+++ VirtualTourist/GCDBlackBox.swift
@@ -0,0 +1,15 @@
+//
+//  GCDBlackBox.swift
+//  VirtualTourist
+//
+//  Created by Shane Sealy on 3/4/19.
+//  Copyright © 2019 Shane Sealy. All rights reserved.
+//
+
+import Foundation
+
+func performUIUpdatesOnMain(_ updates: @escaping () -> Void) {
+    DispatchQueue.main.async {
+        updates()
+    }
+}
