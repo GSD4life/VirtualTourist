ced6fcf5c29d02d4f15fa83f7ad11715d1416586
Shane Sealy <35928028+GSD4life@users.noreply.github.com>
2019-03-25 14:19:16 -0400

    Added PhotoCollectionViewCell (new file). Also updated story board to include image view for the collectionViewCell. App is functional.

---
+++ VirtualTourist/Model/PhotoCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoCollectionViewCell.swift
+//  VirtualTourist
+//
+//  Created by Shane Sealy on 3/25/19.
+//  Copyright © 2019 Shane Sealy. All rights reserved.
+//
+
+import UIKit
+
+class PhotoCollectionViewCell: UICollectionViewCell {
+    
+    
+    @IBOutlet weak var virtualTouristImageView: UIImageView!
+    
+    
+}
