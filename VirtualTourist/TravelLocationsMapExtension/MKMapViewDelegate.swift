100e569282562aa39f3bca9eb4ef21b3d08c6f18
Shane Sealy <35928028+GSD4life@users.noreply.github.com>
2019-05-09 11:22:01 -0400

    Created new file MKMapViewDelegate and new group TravelLocationMapExtension. Added new groups - Views and Cells. Moved PhotoCollectionViewCell.swift into Cells group. App is functional

---
+++ VirtualTourist/TravelLocationsMapExtension/MKMapViewDelegate.swift
@@ -0,0 +1,61 @@
+//
+//  MKMapViewDelegate.swift
+//  VirtualTourist
+//
+//  Created by Shane Sealy on 5/9/19.
+//  Copyright © 2019 Shane Sealy. All rights reserved.
+//
+
+import MapKit
+
+extension TravelLocationsMapVC: MKMapViewDelegate {
+    
+    // udacity forums
+    func mapView(_ mapView: MKMapView, regionDidChangeAnimated animated: Bool) {
+        if mapViewRegionDidChangeFromUserInteraction() {
+            let regionToSave = [
+                "mapRegionCenterLat": mapView.region.center.latitude,
+                "mapRegionCenterLon": mapView.region.center.longitude,
+                "mapRegionSpanLatDelta": mapView.region.span.latitudeDelta,
+                "mapRegionSpanLonDelta": mapView.region.span.longitudeDelta
+            ]
+            
+            let _ = UserDefaults.standard.set(regionToSave, forKey: "savedMapRegion")
+            
+        }
+    }
+    
+    func mapView(_ mapView: MKMapView, didSelect view: MKAnnotationView) {
+        mapView.deselectAnnotation(view.annotation, animated: true)
+        
+        guard let pinImage = view.annotation else { return }
+        
+        if editButton.title == "Done" {
+            mapView.removeAnnotation(pinImage)
+        } else {
+            let _ = performSegue(withIdentifier: "PhotoAlbumVC", sender: self)
+        }
+        
+    }
+    
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        
+        let reuseId = "pin"
+        
+        var pinView = mapView.dequeueReusableAnnotationView(withIdentifier: reuseId) as? MKPinAnnotationView
+        
+        if pinView == nil {
+            pinView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: reuseId)
+            pinView!.animatesDrop = true
+            pinView!.canShowCallout = true
+            pinView!.pinTintColor = .red
+            //pinView!.rightCalloutAccessoryView = UIButton(type: .detailDisclosure)
+        }
+        else {
+            pinView!.annotation = annotation
+        }
+        
+        return pinView
+    }
+    
+}
